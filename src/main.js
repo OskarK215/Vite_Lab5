a387bda0149e4271cc72c2fc47faf93852a29bb1
null <null>
2026-06-16 18:00:37 +0200

    complited supabase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const dateObject = document.getElementById('dataUrodzenia');
 const form = document.getElementById("formId")
 form.addEventListener("submit", (e)=>{
   e.preventDefault()
+  
   console.log(form.dataUrodzenia.value)
   const dateOfB = form.dataUrodzenia.value
   const dateOfBjs = dayjs(dateOfB);
@@ -33,3 +34,72 @@ form.addEventListener("submit", (e)=>{
   dialog.setAttribute("class", "bg-gray-200 text-red-600");
   body.append(dialog);
 });
+const fetchArticles = async () => {
+  try {
+  const response = await fetch(
+  'https://vxxgrhjxusksqbpuuttf.supabase.co/rest/v1/article', {
+  headers: {
+  apiKey: 'sb_publishable_QSeaMOUEJS4aYo4PE1XVRg_cgGDQGu3',
+  },
+  });
+  const data = await response.json();
+  console.log(data);
+  return data;
+  } catch (error) {
+  console.error('Fetch error:', error);
+  }
+ };
+fetchArticles().then(data => {
+  const table_body = document.getElementById("table_body")
+  for (const row in data){
+    const tr = document.createElement("tr")
+    const row_data = data[row]
+    const id = document.createElement("td")
+    id.innerHTML = row_data.id
+    const author = document.createElement("td")
+    author.innerHTML = row_data.author
+    const content = document.createElement("td")
+    content.innerHTML = row_data.content
+    const created_at = document.createElement("td")
+    created_at.innerHTML = row_data.created_at
+    const subtitle = document.createElement("td")
+    subtitle.innerHTML = row_data.subtitle
+    const title = document.createElement("td")
+    title.innerHTML = row_data.title
+    tr.append(id,author,content,created_at,subtitle,title)
+    table_body.append(tr)
+  }
+  
+});
+const createNewArticle = async (data) => {
+  try {
+  const response = await fetch('https://vxxgrhjxusksqbpuuttf.supabase.co/rest/v1/article', {
+  method: 'POST',
+  headers: {
+  apiKey: 'sb_publishable_QSeaMOUEJS4aYo4PE1XVRg_cgGDQGu3',
+  'Content-Type' : 'application/json' ,
+  },
+  body: JSON.stringify(data),
+  });
+ 
+  if (response.status !== 201) {
+  throw new Error(`Status: ${response.status}`);
+  }
+  } catch (error) {
+  console.error('Fetch error:' , error);
+  }
+ };
+ const form_add = document.getElementById("new_article")
+ form_add.addEventListener("submit", (e)=>{
+  e.preventDefault()  
+  const data_pack = {
+    author:form_add.author.value,
+    title:form_add.title.value,
+    subtitle:form_add.subtitle.value,
+    content:form_add.content.value
+  }
+  createNewArticle(data_pack).then(data => {
+    console.log(data_pack)
+  })
+  
+ })
